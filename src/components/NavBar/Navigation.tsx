5ffeaf67a4409b570519bb2da05a34b877b6fc79
thenry3 <43651306+thenry3@users.noreply.github.com>
2021-01-19 22:51:22 -0800

    linked to events

--- src/components/NavBar/Navigation.tsx
+++ src/components/NavBar/Navigation.tsx
@@ -23,12 +23,31 @@ const NavLink = styled(Link)`
   }
 `;
 
+const ExtNavLink = styled("a")`
+  text-decoration: none;
+  color: black;
+  padding: 15px 8px;
+  margin-left: 5px;
+  margin-right: 5px;
+  font-size: 14px;
+
+  &:hover {
+    border-bottom: black 2px solid;
+    padding-bottom: 13px;
+  }
+`;
+
 export default function Navigation() {
   return (
     <>
       <Wrapper>
         <NavLink to="/about">About</NavLink>
-        <NavLink to="/events">Events</NavLink>
+        <ExtNavLink
+          target="_blank"
+          href="https://www.notion.so/8d852b6bc0b74005875b721c27d5314d?v=9bcc1a41ea67455fbf0a13eef46a0048"
+        >
+          Events
+        </ExtNavLink>
         <NavLink to="/hope">HOPE</NavLink>
         <NavLink to="/seaclear">SEA CLEAR</NavLink>
         <NavLink to="/staff">Staff</NavLink>
