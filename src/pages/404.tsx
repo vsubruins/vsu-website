8a034e8006d775a5e60608e74dae7e1c10491a7c
thenry3 <43651306+thenry3@users.noreply.github.com>
2021-01-19 21:12:16 -0800

    fixedd 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Wrapper = styled("div")`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function Page404() {
   return (
     <>
-      <p>404</p>
+      <Wrapper>
+        <p>
+          <strong>404 Error!</strong>
+        </p>
+        <p>Invalid URL or this site is under construction :)</p>
+        <Link to="/">Back to Home</Link>
+      </Wrapper>
     </>
   );
 }
